c33780909762082807a3a5730a60bb314922ed10
seregaxxl <seregaxxl@gmail.com>
2022-12-24 15:12:03 +0300

    sliders, and slider autoplay with homework

---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,99 @@
+import Slider from './slider';
+
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev, activeClass, animate, autoplay) {
+        super(container, next, prev);
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
+
+        this.prev.addEventListener('click', () => {
+            for (let i = this.slides.length - 1; i > 0; i-- ) {
+                if (this.slides[i].tagName !== 'BUTTON') {
+                    let active = this.slides[i];
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlides();
+                    break;
+                }
+            }
+            
+
+        });
+    }
+
+    nextSlide() {
+        if (this.slides[1].tagName == 'BUTTON' && this.slides[2].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]);
+            this.container.appendChild(this.slides[0]);
+            this.container.appendChild(this.slides[0]);
+            console.log(1);
+        } else if (this.slides[1].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]);
+            this.container.appendChild(this.slides[0]);
+            console.log(2);
+        } else {
+            this.container.appendChild(this.slides[0]);
+            console.log(3);
+        }
+        this.decorizeSlides();
+    }
+
+    decorizeSlides() {
+        this.slides.forEach((slide, i) => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+        });
+
+        if (!this.slides[0].closest('button')) {
+            this.slides[0].classList.add(this.activeClass);
+        }
+
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+
+    init() {
+        this.container.style.cssText =`
+        display: flex;
+        flex-wrap: wrap;
+        overflow: hidden;
+        align-items: flex-start;
+        `;
+        this.bindTriggers();
+        this.decorizeSlides();
+        if (this.autoplay) {
+            let autoPlayInterval = setInterval(() => this.nextSlide(), 2000);
+            this.slides.forEach(slide => {
+                slide.addEventListener('mouseenter', () => {
+                    clearInterval(autoPlayInterval);
+                });
+            });
+            this.slides.forEach(slide => {
+                slide.addEventListener('mouseleave', () => {
+                    autoPlayInterval = setInterval(() => this.nextSlide(), 2000);
+                });
+            });
+            this.next.addEventListener('mouseenter', () => {
+                clearInterval(autoPlayInterval);
+            });
+            this.next.addEventListener('mouseleave', () => {
+                autoPlayInterval = setInterval(() => this.nextSlide(), 2000);
+            });
+            this.prev.addEventListener('mouseenter', () => {
+                clearInterval(autoPlayInterval);
+            });
+            this.prev.addEventListener('mouseleave', () => {
+                autoPlayInterval = setInterval(() => this.nextSlide(), 2000);
+            });
+
+        }
+    }
+}
--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,10 +1,23 @@
 
 export default class Slider {
-    constructor({page = '', btns = '', next = '', prev = ''} = {}) {
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
+    constructor({
+        container = null,
+        next = null, 
+        prev = null, 
+        btns = null,
+        activeClass = '',
+        animate,
+        autoplay
+        } = {}) {
+        this.container = document.querySelector(container);
+        this.slides = this.container.children;
         this.btns = document.querySelectorAll(btns);
         this.slideIndex = 1;
+        this.next = document.querySelector(next);
+        this.prev = document.querySelector(prev);
+        this.activeClass = activeClass;
+        this.animate = animate;
+        this.autoplay = autoplay;
     }
    
 }
